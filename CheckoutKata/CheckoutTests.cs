6c609dbe36678e869edb41ba58b8f3ea917e2047
choros007 <lukasz@mosquitodigital.co.uk>
2017-11-24 12:18:56 +0000

    pattern for scanned items

--- CheckoutKata/CheckoutTests.cs
+++ CheckoutKata/CheckoutTests.cs
@@ -10,7 +10,7 @@ namespace CheckoutKata
         public void ScanAItemShouldReturn50()
         {
             var chk = new Checkout();
-            chk.Scan("B");
+            chk.Scan("A");
             Assert.AreEqual(50, chk.GetTotal());
         }
 
@@ -21,6 +21,22 @@ namespace CheckoutKata
             chk.Scan("B");
             Assert.AreEqual(30, chk.GetTotal());
         }
+
+        [TestMethod]
+        public void ScanCItemShouldReturn20()
+        {
+            var chk = new Checkout();
+            chk.Scan("C");
+            Assert.AreEqual(20, chk.GetTotal());
+        }
+
+        [TestMethod]
+        public void ScanDItemShouldReturn15()
+        {
+            var chk = new Checkout();
+            chk.Scan("D");
+            Assert.AreEqual(15, chk.GetTotal());
+        }
     }
 
     public class Checkout
@@ -28,8 +44,24 @@ namespace CheckoutKata
         private int _total;
         public void Scan(string sku)
         {
-            _total = 50;
+            if(sku == "A")
+            {
+                _total = 50;
+            }
+            else if(sku == "B")
+            {
+                _total = 30;
+            }
+            else if (sku == "C")
+            {
+                _total = 20;
+            }
+            else if (sku == "D")
+            {
+                _total = 15;
+            }
         }
+
         public int GetTotal()
         {
             return _total;
